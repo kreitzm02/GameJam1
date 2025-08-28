03e237b27c4ffa0dc111ec45e650a05fdf997f80
Rattendroge <cedrikpomsel@gmail.com>
2025-08-28 16:55:05 +0200

    blub

--- Source/GameJam1/Public/ApproachBoxTraps.h
+++ Source/GameJam1/Public/ApproachBoxTraps.h
@@ -24,7 +24,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Approach Trigger Box")
 	UShapeComponent* m_triggerBox = nullptr;
 		
 };
